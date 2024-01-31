da4acd2f4feeb86c8d2a1d23d59e3e6f3329c74e
sanuiitk65 <sknitp65@gmail.com>
2024-02-01 02:47:03 +0530

    started creating admin panel

---
+++ admin/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Topbar from './components/topbar/Topbar';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Topbar/>
+      </div>
+    );
+  }
+}
+
+export default App;
